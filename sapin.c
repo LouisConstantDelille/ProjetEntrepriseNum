7655f4d20deb92d8d76f8e6673f07132ca90bc1c
Louis Constant <louis.delille04@gmail.com>
2023-12-28 15:27:57 +0100

    Première Partie du sapin en C

---
+++ sapin.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void main(void) {
+    
+    int hauteur;
+    int i ;
+    int j;
+    int k;
+
+    printf("Entrez la hauteur du sapin : ");
+    scanf("%d", &hauteur);
+
+    // Dessiner le sapin
+    for(i = 1; i <= hauteur; i++) {
+        // Espace avant les étoiles
+        for(j = i; j < hauteur; j++) {
+            printf(" ");
+        }
+
+       
+
+}
